77d27333f0f74f7b3d1e83f6d3cdc8fb85093f74
yang <2689991790@qq.com>
2023-03-20 01:09:34 +0800

    feat:去除openblock相关，更新依赖suyuan-gui

--- src/renderer/about.jsx
+++ src/renderer/about.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {productName, version} from '../../package.json';
+import {version} from '../../package.json';
 
-import logo from '../icon/OpenBlockDesktop.svg';
+import logo from '../icon/SuYuan.png';
 import styles from './about.css';
 
 const AboutElement = () => (
     <div className={styles.aboutBox}>
         <div><img
-            alt={`${productName} icon`}
+            alt={`SuYuanDeskTop icon`}
             src={logo}
             className={styles.aboutLogo}
         /></div>
         <div className={styles.aboutText}>
-            <h2>{productName}</h2>
+            <h2>SuYuanDeskTop</h2>
             Version {version}
             <table className={styles.aboutDetails}><tbody>
                 {
